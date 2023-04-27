17afa31a6145233a6f1fafdd74c4a43278906ede
mauro <andrekxus299@gmail.com>
2023-04-27 17:39:49 -0500

    work on menu

--- js/file.js
+++ js/file.js
@@ -2,8 +2,28 @@ const cocktail = async () => {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
     const info = await fetch(url);
     const data = await info.json();
-    const category = data.drinks.map((drink) => drink.strCategory);
+    const category = data.drinks.map((drink) => drink);
     console.log(category);
+    return category
 }
 
-cocktail();
+const showMenu = (newLink) => {
+    newLink.map((newLink =>{
+
+        const cocktailsName = document.querySelector(".nameFromCategory");
+        cocktailsName.innerHTML = `${newLink.strCategory}`
+
+        const cocktailTemplate = document.querySelector('.nameFromCategory')
+        const cocktailsClone = cocktailTemplate.cloneNode(true)
+
+        document.querySelector('#addCategory').appendChild(cocktailsClone);
+    }))
+    const cardDelete = document.querySelector('#delete');
+    cardDelete.remove();
+}
+
+const showCategory = async () =>{
+    showMenu(await cocktail());
+}
+
+showCategory()
---
+++ js/showCocktails.js
@@ -0,0 +1,25 @@
+import { cocktailsApi } from './apiConsuptio.js'
+
+const createCocktailCard = (cocktails) => {
+
+    cocktails.map((cocktails) => {
+
+        const cocktailImages = document.querySelector(".showCocktailPicture");
+        cocktailImages.src = cocktails.strDrinkThumb
+
+        const cocktailsName = document.querySelector(".showCocktailName");
+        cocktailsName.innerHTML = cocktails.strDrink
+
+        const cocktailTemplate = document.querySelector('.cocktailCards')
+        const cocktailsClone = cocktailTemplate.cloneNode(true)
+
+        document.querySelector('#cardContent').appendChild(cocktailsClone);
+    })
+
+    const cardDelete = document.querySelector('#delete');
+    cardDelete.remove();
+}
+
+export const showCocktails = async () => {
+    createCocktailCard(await cocktailsApi())
+}
