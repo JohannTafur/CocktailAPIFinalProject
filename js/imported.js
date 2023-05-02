f8a704f3ea851e60d46eee22686bdb314e0e9fef
mauro <andrekxus299@gmail>
2023-05-01 19:11:53 -0500

    create category from js and change the styles for all the page

--- js/imported.js
+++ js/imported.js
@@ -1,7 +1,7 @@
 import { showCocktails } from './showCocktails.js';
 import { alphabetCreate } from './createAlphabet.js';
-import { showCategory } from './file.js'
+import { showCategory } from './showCategory.js'
 
 showCocktails('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a');
-alphabetCreate()
-showCategory()
+alphabetCreate();
+showCategory('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
